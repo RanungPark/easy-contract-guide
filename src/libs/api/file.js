1ab05b3d67f1e159f95edfaf9145ac943d79bf1f
RanungPark <ckehfqkr302@naver.com>
2023-08-23 14:12:35 +0900

    파일 api

---
+++ src/libs/api/file.js
@@ -0,0 +1,34 @@
+import client from './client';
+
+export const contract = ({ userId, pdfFile}) => {
+  const formData = new FormData();
+  formData.append('userId', userId);
+  formData.append('pdfFile', pdfFile);
+  client.post('http://127.0.0.1:8080/file/contract', formData, {
+    headers : {
+      'Content-Type' : 'multipart/form-data'
+    }
+  });
+}
+
+export const building = ({ contractId, pdfFile}) => {
+  const formData = new FormData();
+  formData.append('contractId', contractId);
+  formData.append('pdfFile', pdfFile);
+  client.post('http://127.0.0.1:8080/file/building-register', formData, {
+    headers : {
+      'Content-Type' : 'multipart/form-data'
+    }
+  });
+}
+
+export const register = ({ contractId, pdfFile}) => {
+  const formData = new FormData();
+  formData.append('contractId', contractId);
+  formData.append('pdfFile', pdfFile);
+  client.post('http://127.0.0.1:8080/file/certifiedcopy', formData, {
+    headers : {
+      'Content-Type' : 'multipart/form-data'
+    }
+  });
+}
