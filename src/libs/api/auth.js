71b8c21b7363a2d3ea14dd9bbea4532e9ec029c4
RanungPark <ckehfqkr302@naver.com>
2023-09-05 15:33:19 +0900

    api auth

--- src/libs/api/auth.js
+++ src/libs/api/auth.js
@@ -2,11 +2,11 @@ import client from './client';
 
 
 export const login = ({ email, password}) => {
-  client.post('http://127.0.0.1:8080/login', {email, password});
+  client.post('http://localhost:8080/login', {email, password});
 }
 
 export const register = ({ email, password, allCheck, username}) => {
-  client.post('http://127.0.0.1:8080/signup', { 
+  client.post('http://localhost:8080/signup', { 
     email,
     password, 
     privacy_agreement_yn : allCheck, 
@@ -14,4 +14,6 @@ export const register = ({ email, password, allCheck, username}) => {
   });
 }
 
-export const check = () => client.get('http://127.0.0.1:8080/check');
+export const check = () => client.get('http://localhost:8080/check');
+
+export const logout = () => client.get('http://localhost:8080/login');
