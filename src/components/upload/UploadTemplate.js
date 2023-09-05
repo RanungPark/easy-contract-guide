8c4b31e4178cec6ac600e748eb9b890c578da392
RanungPark <ckehfqkr302@naver.com>
2023-09-05 17:04:16 +0900

    업로드 템플릿

---
+++ src/components/upload/UploadTemplate.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { styled } from 'styled-components';
+import palette from '../../libs/styles/palette';
+import { Link } from 'react-router-dom';
+import { MdArrowBackIosNew } from 'react-icons/md';
+
+const UploadTemplateBlock = styled.div`
+  position: absolute;
+  left: 0;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  background: ${palette.gray[2]};
+  display:flex;
+  flex-direction: column;
+  justify-content:center;
+  align-items: center;
+`;
+
+const WhiteBox = styled.div`
+  .type-area {
+    display: block;
+    padding-bottom: 0.5rem;
+    padding-top: 1rem;
+    text-align: center;
+    font-weight: bold;
+    letter-spacing: 2px;
+  }
+  box-shadow: 0 0 8px rgba(0,0,0,0.025);
+  padding: 2rem;
+  width: 360px;
+  background: white;
+  border-radius: 2px;
+
+  .backPage {
+    color: ${palette.gray[7]};
+
+    &:hover {
+      color: ${palette.gray[6]};
+    }
+  }
+`
+
+const UploadTemplate = ({children, props}) => {
+  return (
+    <UploadTemplateBlock>
+      <WhiteBox>
+      <Link to='/' className='backPage'><MdArrowBackIosNew /></Link>
+        <div className='type-area'>
+          {props}
+        </div>
+        {children}
+      </WhiteBox>
+    </UploadTemplateBlock >
+  );
+};
+
+export default UploadTemplate;
