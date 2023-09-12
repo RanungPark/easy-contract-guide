f95a7485b87b2ee228500796533d9d497498238d
RanungPark <ckehfqkr302@naver.com>
2023-09-12 16:06:59 +0900

    테스트 페이지

---
+++ src/test/pages/TestPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ImgScannerForm from '../components/ImgScannerForm';
+
+const TestPage = () => {
+  return (
+    <div>
+      <ImgScannerForm />
+    </div>
+  );
+};
+
+export default TestPage;
