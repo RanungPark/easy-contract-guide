308749b12af1e88bab13fd04565b519da02ab41c
RanungPark <ckehfqkr302@naver.com>
2023-09-12 20:16:05 +0900

    standard 페이지 추가

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import ContractListPage from './pages/ContractListPage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import BuildingUploadPage from './pages/BuildingUploadPage';
 import RegisterUploadPage from './pages/RegisterUploadPage';
 import ContractUploadPage from './pages/ContractUploadPage';
 import TestPage from './test/pages/TestPage';
+import StandardPage from './pages/StandardPage';
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<LoginPage />}/>
-      <Route path='/register' element={<RegisterPage />}/>
-      <Route path='/main' element={<ContractListPage />}/>
+      <Route path='/' element={<LoginPage />} />
+      <Route path='/register' element={<RegisterPage />} />
+      <Route path='/standard' element={<StandardPage />} />
       <Route path='/upload/building' element={<BuildingUploadPage />} />
       <Route path='/upload/register' element={<RegisterUploadPage />} />
       <Route path='/upload/contract' element={<ContractUploadPage />} />
