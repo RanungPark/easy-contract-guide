3e20ed36cdcad316e52e27236cebfe9db38b1015
RanungPark <ckehfqkr302@naver.com>
2023-09-14 01:14:52 +0900

    헤더 컨테이너

--- src/components/common/HeaderContainer.js
+++ src/components/common/HeaderContainer.js
@@ -4,14 +4,18 @@ import { useDispatch, useSelector } from 'react-redux';
 import { logout } from '../../modules/user';
 
 const HeaderContainer = () => {
-  const {user} = useSelector(({user}) => ({user: user.user}));
+  const {user} = useSelector(({user}) => 
+  ({
+    user: user.user,
+  }));
+
   const dispatch = useDispatch();
-  const onLogout = () => {
-    dispatch(logout());
-  };
+
 
   return (
-    <Header user={user} onLogout={onLogout}/>
+    <Header 
+      user={user}
+    />
   );
 };
 
