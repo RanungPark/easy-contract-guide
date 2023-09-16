321508560178cd4aafda2a196d3bf96905b9366e
RanungPark <ckehfqkr302@naver.com>
2023-09-16 13:51:53 +0900

    표준계약서 뷰 페이지

---
+++ src/pages/StandardViewPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import StandardView from '../components/view/StandardView';
+
+const StandardViewPage = () => {
+  return (
+    <StandardView />
+  );
+};
+
+export default StandardViewPage;
