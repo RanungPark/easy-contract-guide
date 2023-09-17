62f92d03287a3818ffb69a798141889d339f621a
RanungPark <ckehfqkr302@naver.com>
2023-09-17 19:43:19 +0900

    파일 이름 랩퍼

--- src/components/file/FileViewTemplate.js
+++ src/components/file/FileViewTemplate.js
@@ -1,15 +1,37 @@
 import React from 'react';
 import { styled } from 'styled-components';
+import palette from '../../libs/styles/palette';
 
 const FileViewTemplateBlock = styled.div`
   margin: 2rem 0.5rem;
   box-shadow: 0 0 8px rgba(0,0,0,0.025);
 `;
 
-const FileViewTemplate = ({children}) => {
+const Wrapper = styled.div`
+  .type-area {
+    display: block;
+    padding-bottom: 0.5rem;
+    padding-top: 1rem;
+    text-align: center;
+    font-weight: bold;
+    letter-spacing: 2px;
+    font-size: 1.2rem;
+    color: ${palette.gray[7]};
+  }
+  box-shadow: 0 0 8px rgba(0,0,0,0.025);
+  padding: 2rem;
+  border-radius: 2px;
+`
+
+const FileViewTemplate = ({children, props}) => {
   return (
     <FileViewTemplateBlock>
-      {children}
+      <Wrapper>
+        <div  className='type-area'>
+          {props}
+        </div>
+        {children}
+      </Wrapper>
     </FileViewTemplateBlock >
   );
 };
