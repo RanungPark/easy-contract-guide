38e13b7d32dfe5fec7cdb44c89ea33947ec68691
RanungPark <ckehfqkr302@naver.com>
2023-09-22 15:07:36 +0900

    주소변경

--- src/App.js
+++ src/App.js
@@ -8,13 +8,17 @@ import ContractUploadPage from './pages/ContractUploadPage';
 import TestPage from './test/pages/TestPage';
 import StandardViewPage from './pages/StandardViewPage';
 import GuidePage from './pages/GuidePage';
+import BuildingViewPage from './pages/BuildingViewPage';
+import RegisterViewPage from './pages/RegisterViewPage';
 
 const App = () => {
   return (
     <Routes>
       <Route path='/' element={<LoginPage />} />
-      <Route path='/register' element={<RegisterPage />} />
+      <Route path='/signup' element={<RegisterPage />} />
       <Route path='/standard' element={<StandardViewPage />} />
+      <Route path='/building' element={<BuildingViewPage />} />
+      <Route path='/register' element={<RegisterViewPage />} />
       <Route path='/upload/building' element={<BuildingUploadPage />} />
       <Route path='/upload/register' element={<RegisterUploadPage />} />
       <Route path='/upload/contract' element={<ContractUploadPage />} />
