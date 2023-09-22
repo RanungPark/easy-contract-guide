947e829560ed45cdc088153d3080395b65d7b06c
RanungPark <ckehfqkr302@naver.com>
2023-09-22 15:07:02 +0900

    건축물대장 뷰 페이지

---
+++ src/pages/BuildingViewPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import BuildingView from '../components/view/BuildingView';
+
+const BuildingViewPage = () => {
+  return (
+    <BuildingView />
+  );
+};
+
+export default BuildingViewPage;
