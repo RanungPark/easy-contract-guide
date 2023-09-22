69201435a4153bd50727bf433a2feda1fb381232
RanungPark <ckehfqkr302@naver.com>
2023-09-22 15:06:11 +0900

    더미 이미지 추가

--- src/components/Main/StandardMain.js
+++ src/components/Main/StandardMain.js
@@ -48,11 +48,15 @@ const StandardMain = () => {
       <Wrapper>
       {file === null ? 
         <>
-          <Dummy />
           <FileViewTemplate props={'표준계약서'}>
             <StandardFileForm />
           </FileViewTemplate>
-          <Dummy />
+          <FileViewTemplate props={'나의 계약서'}>
+            <Dummy imageUrls={[`https://weaver-contractproject-159651426016-ap-northeast-2.s3.ap-northeast-2.amazonaws.com/897d7f77-0739-4bc8-9ffe-bc6f38c0b673_KakaoTalk_20230921_165510932_02%20%282%29.pdf-3.png`]}/>
+          </FileViewTemplate>
+          <AnalyzeTemplate props={'표준계약서와 비교 결과'}>
+            <ContractResultForm />
+          </AnalyzeTemplate>
         </> 
         : 
         <>
