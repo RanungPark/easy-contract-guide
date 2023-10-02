d7bcbb7460b4035f555317500c6c8c0bcdde13c6
RanungPark <ckehfqkr302@naver.com>
2023-10-02 21:43:12 +0900

    주소 검색 컴포넌트

---
+++ src/components/common/Address.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { useDaumPostcodePopup } from 'react-daum-postcode';
+import { postcodeScriptUrl } from 'react-daum-postcode/lib/loadPostcode';
+import styled from 'styled-components';
+
+const AddressDiv = styled.div`
+  width: 100%;
+  cursor: pointer;
+`
+
+const Address = ({setAddress, address}) => {
+  const open = useDaumPostcodePopup(postcodeScriptUrl);
+
+  const handleComplete = (data) => {
+    const fullAddress = data.roadAddress + ' (' + data.buildingName + ')'
+    setAddress(fullAddress);    
+  }
+
+  const handleClick = () => {
+  	open({onComplete: handleComplete});
+  }
+
+  return (
+    <>
+      {
+        address === '' ? <> 
+          <AddressDiv onClick={handleClick} className='placeholder'>클릭하여 주소를 검색해주세요</AddressDiv>
+          </> : <AddressDiv onClick={handleClick}>{address}</AddressDiv>
+      }
+    </>
+  );
+};
+
+export default Address;
