091a7f2e683f5fd2d4028466f549447ebd36b0bd
RanungPark <ckehfqkr302@naver.com>
2023-10-02 21:46:33 +0900

    옵션 컴포넌트들 추가

--- src/components/options/OptionTemplate.js
+++ src/components/options/OptionTemplate.js
@@ -3,13 +3,17 @@ import { styled } from 'styled-components';
 import OptionFirst from './OptionFirst';
 import palette from '../../libs/styles/palette';
 import OptionSecond from './OptionSecond';
+import OptionThird from './OptionThird';
+import OptionFourth from './OptionFourth';
+import OptionFifth from './OptionFifth';
+import OptionSixth from './OptionSixth';
+import OptionSeventh from './OptionSeventh';
 
 const OptionTemplateBlock = styled.div`
   height: 93vh;
-  padding: 1rem;
   background: ${palette.gray[3]};
-  width: 25vw;
   display: flex;
+  padding: 1rem;
   align-items: center;
   box-shadow: 0 0 8px rgba(0,0,0,0.025);
   flex-direction: column;
@@ -43,14 +47,18 @@ const OptionHeader = styled.div`
 `
 
 const OptionWrapper = styled.div`
-  width:20vw;
+  width:21vw;
   margin-top: 1rem;
+
+  &.marginTopNone{
+    margin-top: 0;
+  }
 `
 
 const OptionTemplate = () => {
   return (
     <OptionTemplateBlock>
-       <OptionWrapper>
+       <OptionWrapper className='marginTopNone'>
         <OptionHeader>
           <div>집주인 및 세입자</div>
           <div className='page'>1<span className='opacity'> / 7</span></div>
@@ -69,35 +77,35 @@ const OptionTemplate = () => {
           <div>보증금 지급</div>
           <div className='page'>3<span className='opacity'> / 7</span></div>
         </OptionHeader>
-        <OptionFirst />
+        <OptionThird />
        </OptionWrapper>
        <OptionWrapper>
         <OptionHeader>
           <div>특약사항</div>
           <div className='page'>4<span className='opacity'> / 7</span></div>
         </OptionHeader>
-        <OptionFirst />
+        <OptionFourth />
        </OptionWrapper>
        <OptionWrapper>
         <OptionHeader>
           <div>부동산의 표시</div>
           <div className='page'>5<span className='opacity'> / 7</span></div>
         </OptionHeader>
-        <OptionFirst />
+        <OptionFifth />
        </OptionWrapper>
        <OptionWrapper>
         <OptionHeader>
           <div>집주인 상세정보</div>
           <div className='page'>6<span className='opacity'> / 7</span></div>
         </OptionHeader>
-        <OptionFirst />
+        <OptionSixth />
        </OptionWrapper>
        <OptionWrapper>
         <OptionHeader>
           <div>세입자 상세정보</div>
           <div className='page'>7<span className='opacity'> / 7</span></div>
         </OptionHeader>
-        <OptionFirst />
+        <OptionSeventh />
        </OptionWrapper>
     </OptionTemplateBlock >
   );
