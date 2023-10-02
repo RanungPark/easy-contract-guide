e3a2f1c54dac6db1912bd0f53811e7f3793c6065
RanungPark <ckehfqkr302@naver.com>
2023-10-02 21:45:01 +0900

    첫번째 옵션 스타일 변경

--- src/components/options/OptionFirst.js
+++ src/components/options/OptionFirst.js
@@ -7,30 +7,42 @@ const OptionFirstBlock = styled.div`
 
 const OptionBody = styled.div`
   background: ${palette.gray[1]};
+  font-size: 0.9rem;
 
   .title{
     color: ${palette.gray[6]};
     font-weight: bold;
   }
-  .radioItem {
-    margin-right: 0.5rem;
-    
-  }
-  .radioBox {
-    display: flex;
-  }
-  font-size: 0.9rem;
 `
-
 const RadioWrapper = styled.div`
   padding: 0.7rem;
+  padding-bottom: 0;
+
+  &.paddingBottomAdd {
+  padding-bottom: 0.7rem;
+  }
+`
+
+const RadioBox = styled.div`
+  display: flex;
+  margin-top: 0.7rem;
+
+  input {
+    &.marginLeftAdd {
+      margin-left: 2rem;
+    }
+  }
 `
 
 const InputBox = styled.div`
-  margin-top: 1rem;
+  margin-top: 0.7rem;
   background: white;
   border: 0.5px solid #3DA5F5;
   border-radius: 25px;
+  overflow: hidden;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 
   input,
   label {
@@ -39,22 +51,25 @@ const InputBox = styled.div`
     font-size: 1rem;
     padding: 0.5rem;
   } 
-
-  input {
+  
+  input{
+    margin-right:0.5rem;
+    max-width:100%;
     min-width: 0;
     flex: 1;
     background: white;
     color: ${palette.gray[7]};
     &::placeholder {
       color: #a0a0a0;
-      font-size: 0.9rem;   
+      font-size: 0.9rem;
     }
   }
-
+  
   label {
     font-size: 0.9rem;
   }
 `
+
 const OptionFirst = () => {
   const [hostRadio, setHostRadio] = useState(false);
   const [tenantRadio, setTenantRadio] = useState(false);
@@ -67,14 +82,6 @@ const OptionFirst = () => {
 
   const handleTenantClick = (e) => {
     setTenantRadio(e.target.value);
-  }
-
-  const handleHostChange = (e) => {
-    setHostInput(e.target.value);
-  }
-
-  const handleTenantChange = (e) => {
-    setTenantInput(e.target.value);
   }
 
   useEffect(() => {
@@ -90,54 +97,95 @@ const OptionFirst = () => {
       <OptionBody>
         <RadioWrapper>
           <div className='title'>집주인</div>
-          <div className='radioBox'>
-          <div className='radioItem'>
+          <RadioBox>
             <input type="radio" id="host법인" name="host" value="법인명" onClick={handleHostClick}/>
             <label for="host법인">법인</label>
-          </div>
-          <div className='radioItem'>
-            <input type="radio" id="host개인사업자" name="host" value="상호" onClick={handleHostClick}/>
+            <input className="marginLeftAdd" type="radio" id="host개인사업자" name="host" value="상호(사업체명)" onClick={handleHostClick}/>
             <label for="host개인사업자">개인사업자</label>
-          </div>
-          <div className='radioItem'>
-            <input type="radio" id="host개인" name="host" value="성명" onClick={handleHostClick}/>
+            <input className="marginLeftAdd" type="radio" id="host개인" name="host" value="성명" onClick={handleHostClick}/>
             <label for="host개인">개인</label>
-          </div>
-          </div>
+          </RadioBox>
           {
             hostRadio !== false && <>
               <InputBox>
                 <label htmlFor='hostInput'>{hostRadio}</label>
-                <input id="hostInput" type='text' placeholder='입력' value={hostInput} onChange={handleHostChange}/>
+                {
+                  hostRadio === '법인명' ? 
+                    <input
+                      id="tenantInput"
+                      type='text'
+                      placeholder='법인명 입력'
+                      value={hostInput} 
+                      onChange={e => setTenantInput(e.target.value)}
+                    />
+                    : (hostRadio === '상호(사업체명)' ?
+                    <input
+                      id="tenantInput"
+                      type='text'
+                      placeholder='상호(사업체명) 입력'
+                      value={hostInput} 
+                      onChange={e => setTenantInput(e.target.value)}
+                    />
+                    : 
+                    <input
+                    id="tenantInput"
+                    type='text'
+                    placeholder='성명 입력'
+                    value={hostInput} 
+                    onChange={e => setTenantInput(e.target.value)}
+                    />
+                    )
+                }
               </InputBox>
             </>
           }
         </RadioWrapper>
-        <RadioWrapper>
-          <div className='title'>세입자</div>
-          <div className='radioBox'>
-          <div className='radioItem'>
-            <input type="radio" id="tenant법인" name="tenant" value="법인" onClick={handleTenantClick}/>
+        
+        <RadioWrapper className='paddingBottomAdd'>
+          <RadioBox>
+            <input type="radio" id="tenant법인" name="tenant" value="법인명" onClick={handleTenantClick}/>
             <label for="tenant법인">법인</label>
-          </div>
-          <div className='radioItem'>
-            <input type="radio" id="tenant개인사업자" name="tenant" value="상호" onClick={handleTenantClick}/>
+            <input  className="marginLeftAdd" type="radio" id="tenant개인사업자" name="tenant" value="상호(사업체명)" onClick={handleTenantClick}/>
             <label for="tenant개인사업자">개인사업자</label>
-          </div>
-          <div className='radioItem'>
-            <input type="radio" id="tenant개인" name="tenant" value="개인" onClick={handleTenantClick}/>
+            <input  className="marginLeftAdd" type="radio" id="tenant개인" name="tenant" value="성명" onClick={handleTenantClick}/>
             <label for="tenant개인">개인</label>
-          </div>
-          </div>  
+          </RadioBox>
           {
             tenantRadio !== false && <>
               <InputBox>
                 <label htmlFor='tenantInput'>{tenantRadio}</label>
-                <input id="tenantInput" type='text' placeholder='입력' value={tenantInput} onChange={handleTenantChange}/>
+                {
+                  tenantRadio === '법인명' ? 
+                    <input
+                      id="tenantInput"
+                      type='text'
+                      placeholder='법인명 입력'
+                      value={tenantInput} 
+                      onChange={e => setTenantInput(e.target.value)}
+                    />
+                    : (tenantRadio === '상호(사업체명)' ?
+                    <input
+                      id="tenantInput"
+                      type='text'
+                      placeholder='상호(사업체명) 입력'
+                      value={tenantInput} 
+                      onChange={e => setTenantInput(e.target.value)}
+                    />
+                    : 
+                    <input
+                    id="tenantInput"
+                    type='text'
+                    placeholder='성명 입력'
+                    value={tenantInput} 
+                    onChange={e => setTenantInput(e.target.value)}
+                    />
+                    )
+                }
               </InputBox>
             </>
           }
         </RadioWrapper>
+
       </OptionBody>
     </OptionFirstBlock >
   );
