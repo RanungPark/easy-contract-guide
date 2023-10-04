d4682f622827cfb34425502bf071fe899ecbc015
RanungPark <ckehfqkr302@naver.com>
2023-10-05 01:36:25 +0900

    테이블 수정 및 스타일

--- src/components/docs/Payment.js
+++ src/components/docs/Payment.js
@@ -34,30 +34,60 @@ const PaymentTableBodyLeft = styled.div`
   justify-content: center;
   align-items: center;
   width: 6vw;
-  height:4vh;
+  min-height:4vh;
   white-space: pre-wrap;
   margin:0;
   font-weight: bold;
 `
 const PaymentTableBodyRight = styled.div`
   display:flex;
-  justify-content: space-between;
   white-space: pre-wrap;
   margin:0;
   width: 100%;
   align-items:center;
-  padding-left: 1rem;
   padding-right: 1rem;
   color: ${palette.gray[7]};
   border-bottom : 1px solid #3DA5F5;
   border-right : 1px solid #3DA5F5;
   font-weight: bold;
   font-size: 0.95rem;
+  min-height:4vh;
 
   span {
     font-size: 0.75rem;
   }
 `
+
+const Bodyleft = styled.div`
+  width: 30%;
+  display: flex;
+  word-break:break-all;
+  padding-right: 1rem;
+  height: 100%;
+  
+  div {
+    display: flex;
+    align-items: center;
+    padding-left: 1rem;
+  }
+`
+
+const BodyRight = styled.div`
+  width: 70%;
+  display: flex; 
+  word-break:break-all;
+  height: 100%;
+  
+  div {
+    display: flex;
+    align-items: center;
+  }
+
+  .fontChange {
+    font-size: 0.9rem;
+  }
+`
+
 
 const Payment = () => {
   const {
@@ -110,8 +140,12 @@ const Payment = () => {
             <div>보 증  금</div>
           </PaymentTableBodyLeft>
           <PaymentTableBodyRight>
-            <div>금</div>
-            <div>원정 (₩                         )</div>
+            <Bodyleft>
+              <div>금</div>
+            </Bodyleft>
+            <BodyRight>
+              <div>원정 (     ₩{subsidy}      )원</div>
+            </BodyRight>
           </PaymentTableBodyRight>
         </PaymentTableBody>
       </> : <></>}
@@ -122,10 +156,17 @@ const Payment = () => {
             <div>계 약  금</div>
           </PaymentTableBodyLeft>
           <PaymentTableBodyRight>
-            <div>금</div>
-            <div>
-              <span>원정은 계약시에 지급하고 영수함</span>, 영수자(                 <span>(인)</span>)
-            </div>
+            <Bodyleft>
+              <div>금</div>
+              <div>
+                {downPayment}원
+              </div>
+            </Bodyleft>
+            <BodyRight>
+              <div className='fontChange'>
+                <span>원정은 계약시에 지급하고 영수함</span>, 영수자(   <>{payer}</>   <span>(인)</span>)
+              </div>
+            </BodyRight>
           </PaymentTableBodyRight>
         </PaymentTableBody>
       </> : <></>}
@@ -136,20 +177,41 @@ const Payment = () => {
             <div>중 도  금</div>
           </PaymentTableBodyLeft>
           <PaymentTableBodyRight>
-            <div>금</div>
-            <div>원정은     년      월      일에지급하며</div>
+            <Bodyleft>
+              <div>금</div>
+              <div>
+                {middlePayment}원
+              </div>
+            </Bodyleft>
+            <BodyRight>
+              <div>
+                원정은
+                {middlePaymentDay !== '' ? <>{middlePaymentDay}</> : <>   년    월    일</>}
+                에지급하며
+              </div>
+            </BodyRight>
           </PaymentTableBodyRight>
         </PaymentTableBody>
       </> : <></>}
-
       { balance !== '' || balancePaymentDay !== '' ? <>
         <PaymentTableBody>
           <PaymentTableBodyLeft>
             <div>잔       금</div>
           </PaymentTableBodyLeft>
           <PaymentTableBodyRight>
-            <div>금</div>
-            <div>원정은     년      월      일에지급한다</div>
+            <Bodyleft>
+              <div>금</div>
+              <div>
+                {balance}원
+              </div>
+            </Bodyleft>
+            <BodyRight>
+              <div>
+                원정은
+                {balancePaymentDay !== '' ? <>{balancePaymentDay}</> : <>   년    월    일</>}
+                에지급하며
+              </div>
+            </BodyRight>
           </PaymentTableBodyRight>
         </PaymentTableBody>
       </> : <></>}
@@ -160,12 +222,23 @@ const Payment = () => {
             <div>차       임</div>
           </PaymentTableBodyLeft>
           <PaymentTableBodyRight>
-            <div>금</div>
-            <div>원정은 (선불로⋅후불로) 매월      일에지급한다</div>
+
+            <Bodyleft>
+              <div>금</div>
+              <div>
+                {charge}원
+              </div>
+            </Bodyleft>
+            <BodyRight>
+              <div>
+                원정은
+                  {paid !== '' ? <> {paid}로 </> : <> (선불로⋅후불로) </>}
+                매월 {chargePaymentDate}일에지급한다
+              </div>
+            </BodyRight>
           </PaymentTableBodyRight>
         </PaymentTableBody>
       </> : <></>}
-
     </PaymentBlock >
   );
 };
