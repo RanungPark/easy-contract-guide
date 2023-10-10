14439eb878985aa20ce9ed461bc775a1032f9708
RanungPark <ckehfqkr302@naver.com>
2023-10-10 15:27:48 +0900

    페이지 삭제

--- src/App.js
+++ src/App.js
@@ -2,12 +2,8 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
-import BuildingUploadPage from './pages/BuildingUploadPage';
-import RegisterUploadPage from './pages/RegisterUploadPage';
-import ContractUploadPage from './pages/ContractUploadPage';
 import TestPage from './test/pages/TestPage';
 import StandardViewPage from './pages/StandardViewPage';
-import GuidePage from './pages/GuidePage';
 import BuildingViewPage from './pages/BuildingViewPage';
 import RegisterViewPage from './pages/RegisterViewPage';
 import DousPage from './pages/DousPage';
@@ -20,11 +16,7 @@ const App = () => {
       <Route path='/standard' element={<StandardViewPage />} />
       <Route path='/building' element={<BuildingViewPage />} />
       <Route path='/register' element={<RegisterViewPage />} />
-      <Route path='/upload/building' element={<BuildingUploadPage />} />
-      <Route path='/upload/register' element={<RegisterUploadPage />} />
-      <Route path='/upload/contract' element={<ContractUploadPage />} />
       <Route path='/dous' element={<DousPage />} />
-      <Route path='/guide' element={<GuidePage />} />
       <Route path='/test' element={<TestPage />} />
     </Routes>
   );
