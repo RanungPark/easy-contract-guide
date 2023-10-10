0620a6a17dbe61aa1f8ec5a6c1ecd15b18c68bf2
RanungPark <ckehfqkr302@naver.com>
2023-10-10 18:39:59 +0900

    메뉴 블러 수정

--- src/components/menu/MenuBlur.js
+++ src/components/menu/MenuBlur.js
@@ -5,24 +5,41 @@ const MenuBlurBlock = styled.div`
   position: absolute;
   height: 93vh;
   z-index: 100;
-  background-color: rgba(0,0,0,0.3);
+  background-color:  rgba(0, 0, 0, 0.5);
   width:23.7vw;
   display: flex;
   justify-content: center;
   align-items: center;
-
-  div{
-    font-size: 1rem;
-    opacity: 0.3;
-  }
 `;
+
+const ErrorBox = styled.div`
+  .error-massage {
+    display: block;
+    padding-bottom: 2rem;
+    text-align: center;
+    font-weight: bold;
+    letter-spacing: 2px;
+    font-size: 1rem;
+  }
+  box-shadow: 0 0 8px rgba(0,0,0,0.025);
+  padding: 2rem;
+  width: 360px;
+  background: rgb(240,240,240);
+  border-radius: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  opacity: 0.7;
+`
+
 
 const MenuBlur = ({props}) => {
   return (
     <MenuBlurBlock>
-      <div>
+      <ErrorBox>
         {props}
-      </div>
+      </ErrorBox>
     </MenuBlurBlock >
   );
 };
