7f3cfaf84b5f2ca71c2eeb4259360c14fb024f04
RanungPark <ckehfqkr302@naver.com>
2023-10-10 18:39:50 +0900

    타입에 따른 rate변환

--- src/components/comment/PieChartForm.js
+++ src/components/comment/PieChartForm.js
@@ -7,7 +7,7 @@ import palette from '../../libs/styles/palette';
 
 const PieChartFormBlock = styled.div`
   width: 50%;
-  height: 15rem;
+  height: 100%;
   flex-direction:column;
   display: flex;
   align-items: center;
@@ -32,13 +32,17 @@ const PieChartWrapper = styled.div`
   margin-bottom: 1rem;
 `
 
-const PieChartForm = () => {
-  const {user, contractResult} = useSelector(({user, result})=>({
+const PieChartForm = ({type}) => {
+  const {user, contractResult, registerResult, buildingResult} = useSelector(({user, result})=>({
     user: user.user,
     contractResult: result.contractResult,
+    registerResult: result.registerResult,
+    buildingResult: result.buildingResult,
   }));
 
-  const {result} = contractResult;
+  const contract_result = contractResult.result;
+  const register_result = registerResult.result;
+  const building_result = buildingResult.result;
 
 
   const today = new Date();
@@ -48,35 +52,103 @@ const PieChartForm = () => {
 
   return (
     <PieChartFormBlock>
-      <PieChartHeader>
-       {user.username}님의 안전도 퍼센트
-      </PieChartHeader>
-      <PieChartComment>
-      <HiSpeakerphone/> {year}년 {month}월 {date}일 기준으로 안전도 퍼센트,<br/>
-      언제든지 변경 될 수 있기 때문에 주기적인 확인이 필요해요.
-      </PieChartComment>
-      <PieChartWrapper>
-        <PieChart
-          data={[
-            {
-            value: result.rate,
-            color: '#8fc9f5',
-            name: 'name1',
-            },
-          ]}
-          reveal={result.rate}
-          lineWidth={18}
-          background='#dee2e6'
-          rounded
-          animate
-          label={({dataEntry}) => dataEntry.value + '%'}
-          labelStyle={{
-          fontSize: "26px",
-          fill: "#33333"
-          }}
-          labelPosition={0}
-        />
-      </PieChartWrapper>
+      {
+        type === 'standard' ? <>
+          <PieChartHeader>
+            {user.username}님의 표준계약서와의 안전도 퍼센트
+          </PieChartHeader>
+          <PieChartComment>
+            <HiSpeakerphone/> {year}년 {month}월 {date}일 기준으로 안전도 퍼센트,<br/>
+            언제든지 변경 될 수 있기 때문에 주기적인 확인이 필요해요.
+          </PieChartComment>
+          <PieChartWrapper>
+            <PieChart
+              data={[
+                {
+                value: contract_result.rate,
+                color: '#8fc9f5',
+                name: 'name1',
+                },
+              ]}
+              reveal={contract_result.rate}
+              lineWidth={18}
+              background='#dee2e6'
+              rounded
+              animate
+              label={({dataEntry}) => dataEntry.value + '%'}
+              labelStyle={{
+              fontSize: "26px",
+              fill: "#33333"
+              }}
+              labelPosition={0}
+            />
+          </PieChartWrapper>
+        </> : (
+          type === 'register' ? <>
+            <PieChartHeader>
+              {user.username}님의 등기부등본과의 안전도 퍼센트
+            </PieChartHeader>
+            <PieChartComment>
+              <HiSpeakerphone/> {year}년 {month}월 {date}일 기준으로 안전도 퍼센트,<br/>
+              언제든지 변경 될 수 있기 때문에 주기적인 확인이 필요해요.
+            </PieChartComment>
+            <PieChartWrapper>
+              <PieChart
+                data={[
+                  {
+                  value: register_result.rate,
+                  color: '#8fc9f5',
+                  name: 'name1',
+                  },
+                ]}
+                reveal={register_result.rate}
+                lineWidth={18}
+                background='#dee2e6'
+                rounded
+                animate
+                label={({dataEntry}) => dataEntry.value + '%'}
+                labelStyle={{
+                fontSize: "26px",
+                fill: "#33333"
+                }}
+                labelPosition={0}
+              />
+            </PieChartWrapper>
+          </> : (
+            type === 'building' ? <>
+            <PieChartHeader>
+              {user.username}님의 건축물대장과의 안전도 퍼센트
+            </PieChartHeader>
+            <PieChartComment>
+              <HiSpeakerphone/> {year}년 {month}월 {date}일 기준으로 안전도 퍼센트,<br/>
+              언제든지 변경 될 수 있기 때문에 주기적인 확인이 필요해요.
+            </PieChartComment>
+            <PieChartWrapper>
+              <PieChart
+                data={[
+                  {
+                  value: building_result.rate,
+                  color: '#8fc9f5',
+                  name: 'name1',
+                  },
+                ]}
+                reveal={building_result.rate}
+                lineWidth={18}
+                background='#dee2e6'
+                rounded
+                animate
+                label={({dataEntry}) => dataEntry.value + '%'}
+                labelStyle={{
+                fontSize: "26px",
+                fill: "#33333"
+                }}
+                labelPosition={0}
+              />
+            </PieChartWrapper>
+          </> : <></>
+          )
+        )
+      }
     </PieChartFormBlock >
   );
 };
